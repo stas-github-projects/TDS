48f3ca062028c5acf5c0460d40320c4e4c7322cb
stas-github-projects <thatsmytempmail@gmail.com>
2016-11-21 14:21:21 +0300

    1

---
+++ TinyDocumentStorage/Globals.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TinyDocumentStorage
+{
+    internal static class Globals
+    {
+        internal static Service _service = new Service();
+        internal static HashFNV _hash = new HashFNV();
+        internal static DataTypeSerializer _dataserializer = new DataTypeSerializer();
+    }
+}
